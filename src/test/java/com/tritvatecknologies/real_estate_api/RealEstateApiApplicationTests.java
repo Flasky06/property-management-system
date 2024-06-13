15bdd4862d84115e3761fd35a8f004009d15a59d
flasky06 <bonnienjuguna106@gmail.com>
2024-06-13 10:10:30 +0300

    Initialized project

---
+++ src/test/java/com/tritvatecknologies/real_estate_api/RealEstateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tritvatecknologies.real_estate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
